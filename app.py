6dd0650d6a7b492c0e197c80f481e73a2d38c29c
immanas2001 <manas.r.nayak2001@gmail.com>
2026-09-10 05:33:32 +0530

    Fix template response for current Starlette

--- app.py
+++ app.py
@@ -42,10 +42,9 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request
-        }
+        request=request,
+        name="index.html",
+        context={}
     )
 
 # ==========================
